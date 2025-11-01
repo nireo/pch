5d89ebbf911fa5ecd57c240afc646ade66f9727d
nireo <eemil.lehtikangas1@gmail.com>
2025-11-01 02:27:13 +0200

    added zerolog logging for the server

--- go.mod
+++ go.mod
@@ -19,9 +19,12 @@ require (
 	github.com/gdamore/encoding v1.0.1 // indirect
 	github.com/gdamore/tcell/v2 v2.8.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/rivo/tview v0.42.1-0.20250929082832-e113793670e2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/sys v0.36.0 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/term v0.35.0 // indirect
 )
--- grpc_server.go
+++ grpc_server.go
@@ -7,11 +7,12 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
+	"os"
 	"sync"
 	"time"
 
 	pb "github.com/nireo/pch/pb"
+	"github.com/rs/zerolog"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -37,6 +38,7 @@ type RpcServer struct {
 	store         *Storage
 	activeStreams map[string]pb.ChatService_MessageStreamServer // username -> stream
 	mu            sync.RWMutex
+	logger        zerolog.Logger
 
 	stopChallCleanup chan struct{}
 	challMu          sync.RWMutex
@@ -56,6 +58,7 @@ func NewRpcServer(dbPath string) (*RpcServer, error) {
 		challenges:       make(map[string]challengeEntry),
 		stopChallCleanup: make(chan struct{}),
 	}
+	r.logger = zerolog.New(os.Stderr).With().Timestamp().Logger()
 
 	go r.cleanupAuthChallenges()
 	return r, nil
@@ -81,6 +84,7 @@ func (r *RpcServer) cleanupAuthChallenges() {
 		case <-r.stopChallCleanup:
 			return
 		case t := <-t.C:
+			r.logger.Info().Msg("cleaning up expired auth challenges")
 			r.challMu.Lock()
 			for u, chall := range r.challenges {
 				if chall.expiresAt.Before(t) {
@@ -105,6 +109,8 @@ func (r *RpcServer) Register(
 		return nil, ErrInvalidPrekeySignature
 	}
 
+	r.logger.Info().Str("username", req.Username).Msg("registeration to username")
+
 	// if the user already exists return an auth challenge to prove that they're the one that they
 	// claim to be. They will return an signed version of this in a join message
 	// TODO: create if not exists
@@ -118,6 +124,7 @@ func (r *RpcServer) Register(
 		}
 
 		if err := r.store.StoreUser(userRecord); err != nil {
+			r.logger.Err(err).Str("username", req.Username).Msg("error storing user record")
 			return nil, fmt.Errorf("failed to store user %v", err)
 		}
 	}
@@ -125,6 +132,9 @@ func (r *RpcServer) Register(
 	if len(req.OneTimePrekeys) > 0 {
 		err := r.store.AddOTPs(req.Username, req.OneTimePrekeys)
 		if err != nil {
+			r.logger.Err(err).
+				Str("username", req.Username).
+				Msg("error storing user one-time prekeys")
 			return nil, fmt.Errorf("failed to add prekeys %v", err)
 		}
 	}
@@ -132,6 +142,7 @@ func (r *RpcServer) Register(
 	var authChallenge [32]byte
 	_, err := rand.Read(authChallenge[:])
 	if err != nil {
+		r.logger.Err(err).Msg("failed to generate auth challenge") // this shouldn't really happen
 		return nil, fmt.Errorf("failed to generate auth challenge: %s", err)
 	}
 
@@ -146,6 +157,8 @@ func (r *RpcServer) Register(
 	}
 	r.challMu.Unlock()
 
+	r.logger.Info().Str("username", req.Username).Msg("successful registeration")
+
 	return res, nil
 }
 
@@ -156,6 +169,7 @@ func (r *RpcServer) FetchBundle(
 ) (*pb.FetchBundleResponse, error) {
 	userRecord, err := r.store.GetUser(req.Username)
 	if err != nil {
+		r.logger.Err(err).Str("username", req.Username).Msg("user not found for bundle fetch")
 		return nil, fmt.Errorf("user not found: %s", req.Username)
 	}
 
@@ -170,6 +184,7 @@ func (r *RpcServer) FetchBundle(
 		bundle.OneTimePrekey = otp.PublicKey
 	}
 
+	r.logger.Info().Str("username", req.Username).Msg("user prekey bundle fetched")
 	return &pb.FetchBundleResponse{
 		Bundle: bundle,
 	}, nil
@@ -182,6 +197,7 @@ func (r *RpcServer) UploadOPTs(
 ) (*pb.UploadOTPsResponse, error) {
 	userRecord, err := r.store.GetUser(req.Username)
 	if err != nil {
+		r.logger.Err(err).Str("username", req.Username).Msg("user not found for otp upload")
 		return nil, fmt.Errorf("error getting user: %v", err)
 	}
 
@@ -191,18 +207,28 @@ func (r *RpcServer) UploadOPTs(
 			otp.PublicKey,
 			otp.Signature,
 		) {
+			r.logger.Error().
+				Str("username", req.Username).
+				Msg("invalid otp signature during upload")
 			return nil, fmt.Errorf("invalid otp signature")
 		}
 	}
 
 	if err := r.store.AddOTPs(req.Username, req.OneTimePrekeys); err != nil {
+		r.logger.Err(err).Str("username", req.Username).Msg("error storing uploaded otps")
 		return nil, fmt.Errorf("failed to store otps %v", err)
 	}
 
 	totalNow, err := r.store.CountOTPs(req.Username)
 	if err != nil {
+		r.logger.Err(err).Str("username", req.Username).Msg("error counting otps after upload")
 		return nil, fmt.Errorf("failed to get otp key count: %v", err)
 	}
+
+	r.logger.Info().Str("username", req.Username).
+		Int("uploaded", len(req.OneTimePrekeys)).
+		Int("total", totalNow).
+		Msg("otps uploaded successfully")
 
 	return &pb.UploadOTPsResponse{
 		UploadedCount: int32(len(req.OneTimePrekeys)),
@@ -227,7 +253,7 @@ func (r *RpcServer) cleanupStreamContext(ctx *streamContext) {
 		r.mu.Lock()
 		delete(r.activeStreams, ctx.username)
 		r.mu.Unlock()
-		log.Printf("stream closed for user: %s", ctx.username)
+		r.logger.Info().Str("username", ctx.username).Msg("cleaned up stream context")
 	}
 }
 
@@ -235,12 +261,13 @@ func (r *RpcServer) handleEncryptedMessage(msg *pb.EncryptedMessage, ctx *stream
 	// TODO: if an error occurs here it leads to the issue that the conversion ratcher state is
 	// incremented meaning that it should somehow be rolled back to prevent these issues.
 	if !ctx.streamRegistered {
+		r.logger.Error().
+			Str("username", ctx.username).
+			Msg("client must join before sending messages")
 		return fmt.Errorf("client must join before sending messages")
 	}
 	receiver := msg.ReceiverId
 	sender := ctx.username
-
-	log.Printf("message from %s to %s", sender, receiver)
 
 	r.mu.RLock()
 	recipientStream, ok := r.activeStreams[receiver]
@@ -258,10 +285,11 @@ func (r *RpcServer) handleEncryptedMessage(msg *pb.EncryptedMessage, ctx *stream
 			},
 		})
 		if err != nil {
-			log.Printf("failed to deliver message to %s: %v", receiver, err)
+			r.logger.Err(err).
+				Str("receiver", receiver).
+				Msg("failed to deliver message")
 		}
 	} else {
-		log.Printf("recipient %s offline, ", receiver)
 		payload, err := proto.Marshal(&pb.EncryptedMessage{
 			SenderId:       sender,
 			ReceiverId:     receiver,
@@ -269,7 +297,7 @@ func (r *RpcServer) handleEncryptedMessage(msg *pb.EncryptedMessage, ctx *stream
 			Timestamp:      timestamppb.Now(),
 		})
 		if err != nil {
-			log.Printf("failed to marshal message: %v", err)
+			r.logger.Err(err).Msg("failed to marshal offline message")
 			return err
 		}
 
@@ -280,7 +308,8 @@ func (r *RpcServer) handleEncryptedMessage(msg *pb.EncryptedMessage, ctx *stream
 		}
 		err = r.store.AddUserMessage(offlineMsg, receiver, sender)
 		if err != nil {
-			log.Printf("failed to add user message: %v", err)
+			r.logger.Err(err).Str("receiver", receiver).
+				Msg("failed to store offline message")
 			return fmt.Errorf("failed to add user message: %s", err)
 		}
 	}
@@ -294,21 +323,25 @@ func (r *RpcServer) handleJoinMessage(jreq *pb.JoinRequest, ctx *streamContext) 
 	chall, ok := r.challenges[username]
 	if !ok {
 		r.challMu.Unlock()
+		r.logger.Error().Str("username", username).Msg("no auth challenge found for user")
 		return fmt.Errorf("no auth challenge found for user")
 	}
 	delete(r.challenges, username)
 	r.challMu.RUnlock()
 
 	if chall.expiresAt.Before(time.Now()) {
+		r.logger.Error().Str("username", username).Msg("auth challenge expired")
 		return fmt.Errorf("auth challenge expired try registering again")
 	}
 
 	userRecord, err := r.store.GetUser(username)
 	if err != nil {
+		r.logger.Err(err).Str("username", username).Msg("user not found during join")
 		return fmt.Errorf("user not found: %v", err)
 	}
 
 	if !ed25519.Verify(userRecord.VerifyingKey, chall.challenge[:], jreq.Signature) {
+		r.logger.Error().Str("username", username).Msg("invalid challenge signature")
 		return fmt.Errorf("invalid challenge signature")
 	}
 
@@ -319,11 +352,9 @@ func (r *RpcServer) handleJoinMessage(jreq *pb.JoinRequest, ctx *streamContext) 
 	r.mu.Unlock()
 	ctx.streamRegistered = true
 
-	log.Printf("client joined: %s", username)
-
 	offlineMsgs, err := r.store.GetUserMessages(username)
 	if err != nil {
-		log.Printf("failed to offline messages: %s", err)
+		r.logger.Err(err).Str("username", username).Msg("failed to fetch offline messages")
 		offlineMsgs = nil
 	}
 
@@ -353,13 +384,15 @@ func (r *RpcServer) handleJoinMessage(jreq *pb.JoinRequest, ctx *streamContext) 
 			},
 		},
 	}); err != nil {
-		log.Printf("failed to send join ack: %v", err)
+		r.logger.Err(err).Str("username", username).Msg("failed to send join ack")
+		return err
 	}
 
 	if len(offlineMsgs) > 0 {
-		log.Printf("delivered %d offline messages to %s", len(offlineMsgs), username)
+		r.logger.Info().Str("username", username).Int("count", len(offlineMsgs)).
+			Msg("delivered offline messages")
 		if err := r.store.DeleteUserMessages(username); err != nil {
-			log.Printf("failed to delete offline messages: %v", err)
+			r.logger.Err(err).Str("username", username).Msg("failed to delete offline messages")
 		}
 	}
 
@@ -374,7 +407,7 @@ func (r *RpcServer) handleHeartbeatMessage(msg *pb.HeartbeatMessage, ctx *stream
 			},
 		},
 	}); err != nil {
-		log.Printf("failed to send heartbeat ack: %v", err)
+		r.logger.Err(err).Str("username", ctx.username).Msg("failed to send heartbeat ack")
 		return err
 	}
 	return nil
@@ -382,12 +415,17 @@ func (r *RpcServer) handleHeartbeatMessage(msg *pb.HeartbeatMessage, ctx *stream
 
 func (r *RpcServer) handleKeyExchange(msg *pb.KeyExchangeMessage, ctx *streamContext) error {
 	if !ctx.streamRegistered {
+		r.logger.Error().
+			Str("username", ctx.username).
+			Msg("client must join before key exchange")
 		return fmt.Errorf("client must join before key exchange")
 	}
 	username := ctx.username
 	receiver := msg.ReceiverId
 
-	log.Printf("key exchange from %s to %s", username, receiver)
+	r.logger.Info().Str("username", username).
+		Str("receiver", receiver).
+		Msg("handling key exchange message")
 
 	r.mu.RLock()
 	recipientStream, ok := r.activeStreams[receiver]
@@ -404,10 +442,13 @@ func (r *RpcServer) handleKeyExchange(msg *pb.KeyExchangeMessage, ctx *streamCon
 			},
 		})
 		if err != nil {
-			log.Printf("failed to deliver key exchange to %s: %v", receiver, err)
+			r.logger.Err(err).Str("receiver", receiver).
+				Msg("failed to deliver key exchange")
 		}
 	} else {
-		log.Printf("recipient %s offline for key exchange", receiver)
+		r.logger.Info().Str("username", username).
+			Str("receiver", receiver).
+			Msg("storing offline key exchange message")
 
 		payload, err := proto.Marshal(&pb.KeyExchangeMessage{
 			SenderId:       username,
@@ -416,7 +457,9 @@ func (r *RpcServer) handleKeyExchange(msg *pb.KeyExchangeMessage, ctx *streamCon
 			Timestamp:      timestamppb.Now(),
 		})
 		if err != nil {
-			log.Printf("failed to marshal key exchange: %v", err)
+			r.logger.Info().Str("username", username).
+				Str("receiver", receiver).
+				Msg("failed to marshal offline key exchange message")
 			return err
 		}
 
@@ -427,7 +470,9 @@ func (r *RpcServer) handleKeyExchange(msg *pb.KeyExchangeMessage, ctx *streamCon
 		}
 		err = r.store.AddUserMessage(offlineMsg, receiver, username)
 		if err != nil {
-			log.Printf("failed to store offline key exchange: %v", err)
+			r.logger.Err(err).Str("receiver", receiver).
+				Msg("failed to store offline key exchange message")
+			return fmt.Errorf("failed to add user message: %s", err)
 		}
 	}
 
@@ -445,7 +490,8 @@ func (r *RpcServer) handleMessage(msg *pb.ClientMessage, ctx *streamContext) err
 	case *pb.ClientMessage_KeyExchange:
 		return r.handleKeyExchange(v.KeyExchange, ctx)
 	default:
-		log.Printf("unknown message type from %s: %T", ctx.username, v)
+		r.logger.Error().Str("username", ctx.username).
+			Msgf("unknown message type: %T", v)
 		return fmt.Errorf("unknown message type: %T", v)
 	}
 }
@@ -454,7 +500,7 @@ func (r *RpcServer) handleMessage(msg *pb.ClientMessage, ctx *streamContext) err
 func (r *RpcServer) MessageStream(
 	stream pb.ChatService_MessageStreamServer,
 ) error {
-	log.Println("stream connection initiated")
+	r.logger.Info().Msg("stream connection initiated")
 
 	streamCtx := &streamContext{
 		streamRegistered: false,
@@ -466,18 +512,18 @@ func (r *RpcServer) MessageStream(
 	for {
 		msg, err := stream.Recv()
 		if err == io.EOF {
-			log.Printf("client closed stream normally: %s", streamCtx.username)
+			r.logger.Info().Msgf("client closed stream normally: %s", streamCtx.username)
 			return nil
 		}
 
 		if err != nil {
-			log.Printf("stream error for user %s: %v", streamCtx.username, err)
+			r.logger.Err(err).Str("username", streamCtx.username).Msg("failed to receive message")
 			return err
 		}
 
 		err = r.handleMessage(msg, streamCtx)
 		if err != nil {
-			log.Printf("failed to handle message for user %s: %v", streamCtx.username, err)
+			r.logger.Err(err).Str("username", streamCtx.username).Msg("failed to handle message")
 			return err
 		}
 	}
